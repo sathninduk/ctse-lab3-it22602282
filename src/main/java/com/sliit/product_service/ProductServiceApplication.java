023ae4a7ef16598f9f4eabee0b4126ffba0f0b91
sathninduk <45420836+sathninduk@users.noreply.github.com>
2026-02-14 13:36:16 +0530

    Rename project to product-service and update pom
    Rename package and main/test classes from org.sliit.ctse to com.sliit.product_service (CtseApplication -> ProductServiceApplication) and update application.properties application name. Update pom.xml to new groupId/artifactId/name/description, add spring-boot-starter-data-jpa and H2 (runtime), remove Lombok and its annotation-processor/plugin excludes. Also remove LICENSE file and add .DS_Store; adjust build plugin configuration accordingly.

--- src/main/java/com/sliit/product_service/ProductServiceApplication.java
+++ src/main/java/com/sliit/product_service/ProductServiceApplication.java
@@ -1,13 +1,13 @@
-package org.sliit.ctse;
+package com.sliit.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CtseApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CtseApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/sliit/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/sliit/product_service/ProductServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.sliit.ctse;
+package com.sliit.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CtseApplicationTests {
+class ProductServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
